23792e4120c23ba30b1001d62fc6f60b39156646
Nyambayar S <web-master@tavanbogd.com>
2020-09-29 11:44:42 +0800

    save

---
+++ components/Erxes.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+const Erxes = ({data}) => {
+    useEffect(()=>{
+        window.erxesSettings = { forms: [{ brand_id: data.Brand, form_id: data.Form, }], };
+        var script = document.createElement('script');
+        script.src = "https://erxes.tavanbogd.mn/widgets/build/formWidget.bundle.js";
+        script.async = true;
+        var entry = document.getElementsByTagName('script')[0];
+        entry.parentNode.insertBefore(script, entry);
+    },[])
+    return (
+        <React.Fragment></React.Fragment>
+    );
+};
+
+export default Erxes;
